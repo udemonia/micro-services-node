df6ab63c74896e5bcac884b05d5127aad39e1262
udemonia <brandon.l.lambert@icloud.com>
2021-10-24 09:05:35 -0500

    event bus basics working

--- mini-micro-service/comments/index.js
+++ mini-micro-service/comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { randomBytes } = require('crypto')
 const morgan = require('morgan');
 const cors = require('cors');
+const { default: axios } = require('axios');
 const app = express();
 
 app.use(express.urlencoded({extended: true})); 
@@ -15,7 +16,7 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   console.log(req.body)
   const { content } = req.body;
@@ -25,10 +26,28 @@ app.post('/posts/:id/comments', (req, res) => {
   comments.push({ id: commentId, content });
 
   commentsByPostId[req.params.id] = comments;
-    console.log(commentsByPostId)
+  console.log(commentsByPostId)
+
+  try {
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentCreated',
+      data: {
+        id: commentId,
+        content: content,
+        postId: req.params.id
+      }});
+  } catch (err) {
+    console.log(err)
+  }
+
   res.status(201).send(comments);
 
 });
+
+app.post('/events', (req, res) => {
+  console.log('Event Received', req.body.type);
+  res.send({})
+})
 
 const COMMENTPORT = process.env.COMMENTPORT || 3003
 
--- mini-micro-service/posts/index.js
+++ mini-micro-service/posts/index.js
@@ -3,6 +3,8 @@ const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
+const { default: axios } = require('axios');
+const { application } = require('express');
 const app = express();
 
 app.use(express.urlencoded({extended: true})); 
@@ -16,14 +18,32 @@ app.get('/posts', (req, res) => {
     res.send(posts)
 })
 
-app.post('/posts', (req,res) => {
+app.post('/posts', async (req,res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
+
     posts[id] = {
         id: id,
         title: title
     }
+    try {
+        await axios.post("http://localhost:4005/events", {
+          type: "PostCreated",
+          data: {
+            id,
+            title,
+          },
+        });
+    } catch (error) {
+        console.log(error)
+    }
+
     res.status(201).send(posts[id]);
+})
+
+app.post('/events', (req, res) => {
+    console.log('Event Received', req.body.type);
+    res.send({})
 })
 
 app.listen(4000, () => console.log(`POST Services - Listening on port 4k`))
